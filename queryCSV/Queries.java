d0aa9073cce8f4c7020fbb65263d4d284f032636
Svilen-Angelov <svilen.a.angelov@gmail.com>
2015-02-23 20:37:02 +0200

    First version
    Works fine but I need to work a bit on the formatting.

---
+++ queryCSV/Queries.java
@@ -0,0 +1,166 @@
+package queryCSV;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Queries {
+
+	private BufferedReader br;
+	private String fileLocation;
+
+	public Queries(String fileLocation) {
+
+		this.fileLocation = fileLocation;
+
+	}
+	
+	
+	public void selectQuerie(ArrayList<String> params, int level){
+		
+		try {
+			br = new BufferedReader(new FileReader(fileLocation));
+			String cvsSplitby = ",";
+			String firstLine = br.readLine();
+			String line = "";
+			
+			String columns[] = firstLine.split(cvsSplitby);
+			
+			for(int i = 0; i < columns.length; i++){
+				if(!params.contains(columns[i]))
+					columns[i] = "SKIP";
+				else
+					System.out.print("| " + columns[i] + " |");
+			}
+			System.out.println();
+
+			while(true){
+				line = br.readLine();
+				if(line == null || level == 0)
+					break;
+				String lineparts[] = line.split(cvsSplitby);
+				
+				for(int i = 0; i < lineparts.length; i++)
+					if(!columns[i].equals("SKIP"))
+						System.out.print("| " + lineparts[i] + " |");
+				
+				System.out.println();
+				level--;
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally{
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+	public void sumQuerie(String column) {
+
+		try {
+			br = new BufferedReader(new FileReader(fileLocation));
+			int sum = 0;
+			String cvsSplitBy = ",";
+			String line = br.readLine();
+
+			String[] columns = line.split(cvsSplitBy);
+
+			for (int i = 0; i < columns.length; i++)
+				if (columns[i].equals(column)) // Finds the right column
+					while (true) {
+
+						line = br.readLine();
+						if (line == null)
+							break;
+
+						String[] cols = line.split(cvsSplitBy);
+						try {
+							if(Queries.isInt(cols[i]))
+								sum += Integer.parseInt(cols[i]); // Adds the value of that column to sum
+						} catch (NumberFormatException e) { // If its not a number catches the exception 
+							e.printStackTrace();
+						}
+					}
+
+			System.out.println("The sum is : " + sum);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally{
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+	
+	public void showQuerie(){
+		
+		try {
+			br = new BufferedReader(new FileReader(fileLocation));
+			String line = br.readLine();
+			System.out.println(line);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	public void findQuerie(String x){
+		
+		try {
+			br = new BufferedReader(new FileReader(fileLocation));
+			String line = br.readLine();
+			String cvsSplitBy = ",";
+			
+			while(true){
+				line = br.readLine();
+				if(line == null)
+					break;
+				
+				String[] columns = line.split(cvsSplitBy);
+				
+				for(int i =0; i < columns.length; i++){
+					if(columns[i].contains(x)){
+						for(String column : columns)
+							System.out.print("| " + column + " |");
+						System.out.println();
+						break;
+					}		
+				}				
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	public static boolean isInt(String isInt){
+		return isInt.matches("-?\\d+");
+	}
+
+}
---
+++ queryCSV/myquery.java
@@ -0,0 +1,77 @@
+package queryCSV;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class myquery {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		Queries myquery = new Queries(
+				"C:\\workspace\\HackBulgaria\\src\\queryCSV\\csvDB.csv");
+
+		String line = "";
+		while (!line.equals("exit")) {
+
+			System.out.println("Enter a query(Enter \"exit\" to end): ");
+			line = sc.nextLine();
+			String input[] = line.split(" |,"); // Splits with space or
+												// commas(because of SELECT
+												// id,name example)
+
+			// SELECT
+			if (input[0].equals("SELECT")) {
+
+				ArrayList<String> params = new ArrayList<String>();
+				int limit = -1;
+
+				for (int i = 1; i < input.length; i++) {
+					if (!input[i].equals("LIMIT")) {
+						params.add(input[i]);
+					} else {
+						if (i + 1 < input.length)
+							if (Queries.isInt(input[i + 1]))
+								limit = Integer.parseInt(input[i + 1]);
+							else
+								System.out.println("Invalid limit.");
+						else
+							System.out.println("You forgot to enter a LIMIT");
+					}
+				}
+
+				myquery.selectQuerie(params, limit);// If the method gets sent a
+													// -1 limit it works like
+													// without limit.
+
+				// SUM
+			} else if (input[0].equals("SUM")) {
+
+				if (input.length == 2) // Prevents NullPointer with bad input.
+					myquery.sumQuerie(input[1]);
+				else
+					System.out.println("Invalid sum input.");
+
+				// SHOW
+			} else if (input[0].equals("SHOW")) {
+
+				myquery.showQuerie();
+
+				// FIND
+			} else if (input[0].equals("FIND")) {
+				if (input.length == 2)
+					myquery.findQuerie(input[1]);
+				else
+					System.out.println("Invalid search input.");
+
+				// INVALID
+			} else {
+				System.out.println("Invalid query. The available queries are SELECT,FIND,SUM,SHOW.");
+			}
+		}
+
+		sc.close();
+
+	}
+
+}
